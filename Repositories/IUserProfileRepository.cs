1f881d27e24c31b06f33ac895551edd2e5c35a92
Emily <emily.reed0913@gmail.com>
2022-01-12 09:29:19 -0600

    second commit

---
+++ Repositories/IUserProfileRepository.cs
@@ -0,0 +1,18 @@
+﻿using Streamish.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Streamish.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        List<UserProfile> GetAll();
+        UserProfile GetById(int id);
+        void Add(UserProfile userProfile);
+        void Update(UserProfile userProfile);
+        void Delete(int id);
+        UserProfile GetByIdWithVideos(int id);
+    }
+}
